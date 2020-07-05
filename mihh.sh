1e778e2692fb4dd952d7d511085128013b9987a0
BlastOfMihh <mihneaturcu29@tuta.io>
2020-07-06 00:28:13 +0300

    awesome

--- mihh.sh
+++ mihh.sh
@@ -1,4 +1,16 @@
-setxkbmap -option caps:escape;
-redshift -x; redshift -O 1400 1400;
-xset -b;
+setxkbmap -option caps:escape &
+redshift -x; redshift -O 1400 1400 &
+xset -b &
+optimus-manager &
 
+# eos stuff
+nitrogen --restore &
+# picom -cC &
+picom --experimental-backends &
+# pasystray &
+nm-applet & sleep .1 &
+cbatticon & sleep .1 &
+# blueman-tray &
+
+# #set powersavings for display:
+# exec --no-startup-id xset s 480 dpms 600 600 600
